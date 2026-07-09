8e50f37108cca7125a3b988a5fdaa693f2a7352c
Ashish Modak <toashish2006@gmail.com>
2026-07-09 20:15:16 +0530

    Enhance selenium_driver_Setup.py with comments
    Added comments to explain Selenium WebDriver setup.

--- selenium_driver/selenium_driver_Setup.py
+++ selenium_driver/selenium_driver_Setup.py
@@ -1,9 +1,19 @@
+# The Selenium WebDriver is used to automate the Chrome browser
 from selenium import webdriver
+
+# Automatically downloads and manages the correct ChromeDriver version
 from webdriver_manager.chrome import ChromeDriverManager
+
+# Connects ChromeDriver with Selenium
 from selenium.webdriver.chrome.service import Service
+
+# Used to configure Chrome browser settings
 from selenium.webdriver.chrome.options import Options
 
+
+
 def get_Driver():
+    
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
