657d5dfcaa47c85b1b7c8e4fdd7c59108ed7f2da
Ashish Modak <toashish2006@gmail.com>
2026-07-10 23:24:19 +0530

    Implement menu loop for bot operations
    Added a loop to keep the bot running and display menu options.

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ def main():
     # Display a welcome message when the bot starts 
     print("---------- Your smart web bot is started ----------\n")
 
+    
     # Keep the bot running until the user chooses to exit
     while True:
+
+        # The available menu options 
         print("\n----- Menu Underlisted: -----")
         print("1. Scrape the Jobs")
         print("2. Scrape the Deals")
@@ -19,6 +22,7 @@ def main():
 
         choice = input("\nEnter your choice ----> 1/2/3: ")
 
+        # Handle job scrapping
         if choice == "1":
             print("\nOk, you want to scrape the job details.....\n")
 
